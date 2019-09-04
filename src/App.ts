0d95acb1d77537ea4240de4b9c7fce1246113fbd
Stéphane Souron <stef@vertcitron.fr>
2019-09-04 09:26:32 +0200

    Big Bang

---
+++ src/App.ts
@@ -0,0 +1,6 @@
+const placeholder = document.createElement('div')
+placeholder.className = "placeholder"
+const title = document.createElement('h1')
+title.textContent = 'All My Feeds'
+placeholder.appendChild(title)
+document.body.appendChild(placeholder)
